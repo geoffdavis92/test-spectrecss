55fc91f8951037f3e380c44512cfd117fc254810
Geoff Davis <geoffdavis92@gmail.com>
2016-04-23 00:20:08 -0500

    update modal

--- src/js/modal.spectre.js
+++ src/js/modal.spectre.js
@@ -1,4 +1,4 @@
-// Spectre Modal
+// modal.js
 
 Array.prototype.each = function(fn) {
 	for(let i=0;i<this.length;i++) {
@@ -6,15 +6,19 @@ Array.prototype.each = function(fn) {
 	}
 }
 
-const oTriggers = [], cTriggers = []
+const oTriggers = [], cTriggers = [], overlayArr = []
 const dataOpenModal = document.querySelectorAll('[data-open="modal"]')
 const dataCloseModal = document.querySelectorAll('[data-close="modal"]')
+const overlays = document.querySelectorAll('.modal-overlay')
 
 for(let x=0;x<dataOpenModal.length;x++) {
 	oTriggers.push(dataOpenModal[x])
 }
 for(let x=0;x<dataCloseModal.length;x++) {
 	cTriggers.push(dataCloseModal[x])
+}
+for(let x=0;x<overlays.length;x++) {
+	overlayArr.push(overlays[x])
 }
 
 oTriggers.each(function(el) {
@@ -30,9 +34,17 @@ oTriggers.each(function(el) {
 cTriggers.each(function(el) {
 	el.addEventListener('click',function(e) {
 		e.preventDefault();
-		console.log('close click')
+		// console.log('close click')
 		let modalContainer = el.getAttribute('href')
 		modalContainer = document.querySelector(`.modal${modalContainer}`)
 		modalContainer.setAttribute('class',`modal`)
+	})
+})
+
+overlayArr.each(function(el) {
+	el.addEventListener('click',function(e) {
+		e.preventDefault();
+		let activeModal = document.querySelector('.modal[id].active')
+		activeModal.setAttribute('class',`${activeModal.getAttribute('class').replace('active','')}`)
 	})
 })
